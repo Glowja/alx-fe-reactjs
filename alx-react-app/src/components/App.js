bee77f2f3d2c8aae9788a14059147fff6e261896
Glowja <gloryjibril433@gmail.com>
2024-08-10 23:00:23 +0100

    wecome added

--- alx-react-app/src/components/App.js
+++ alx-react-app/src/components/App.js
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 function App() {
     return (
         <div>
+            "WelcomeMessage"
             <WelcomeMessage />
             {/* Other components or JSX */}
         </div>
